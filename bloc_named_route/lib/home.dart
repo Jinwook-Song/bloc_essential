85dfdbe9c44f781ac7de97df8f7663e3ecf226e9
jinwook song <wlsdnr129@naver.com>
2024-12-03 09:21:24 +0900

    init: named route starter

---
+++ bloc_named_route/lib/home.dart
@@ -0,0 +1,48 @@
+import 'package:bloc_named_route/count.dart';
+import 'package:bloc_named_route/cubit/count/count_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CountCubit(),
+      child: const HomeScreenView(),
+    );
+  }
+}
+
+class HomeScreenView extends StatelessWidget {
+  const HomeScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) => BlocProvider<CountCubit>.value(
+                      value: context.read<CountCubit>(),
+                      child: const CountScreen(),
+                    ),
+                  ));
+                },
+                child: const Text("Count Page")),
+            const SizedBox(height: 40),
+            ElevatedButton(
+                onPressed: context.read<CountCubit>().increment,
+                child: const Text("Increament")),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
